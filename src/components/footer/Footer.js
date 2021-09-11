b7868c7975491ae08925f54282719ffdaa186675
Lê Văn Luân <637745@sv.vnua.edu.vn>
2021-09-11 09:13:38 +0700

    work list view commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            CoppyRight @ by ABC
+        </div>
+    );
+}
+
+export default Footer;
--- src/components/works/WorkList.js
+++ src/components/works/WorkList.js
@@ -1,90 +1,85 @@
 import React from 'react';
 import WorkItem from './WorkItem';
 import Filter from '../filter/Filter';
+import Footer from '../footer/Footer';
+import './WorkList.css';
 
 const WorkList = () => {
     return (
-        <div className="noPadding right_wrap">
-            {/* ------------- content ----------------- */}
-            <div className="main_navbar headding">
-                <h1>
-                    <i className="fa fa-bars" aria-hidden="true" id="toggle_nav" /> Việc nhà
-                </h1>
-            </div>
-            <div className="container main_content">
-                <div className="row">
-
-                    {/* ------- Filter ------- */}
-                    <Filter />
-                    {/* ------- End Filter ------- */}
-
-                    <div className="col-12">
-                        <div className="card" style={{ fontSize: '100%' }}>
-                            <div className="card-header">
-                                <h1 className="card-title">
-                                    <p className="p_text">Danh sách các loại việc nhà</p>
-                                </h1>
-                                <div className="card-tools">
-                                    <div className="input-group input-group-sm">
-                                        <div className="input-group w_ip">
-                                            <input type="text" className="form-control ip" placeholder="Search ..." aria-label="Input group example" aria-describedby="basic-addon1" />
-                                            <span className="input-group-text" id="basic-addon1">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z">
-                                                    </path>
-                                                </svg>
-                                            </span>
+        <div className="container main_content">
+            <div className="row">
+                <Filter />
+                <div className="col-12">
+                    <div className="card" style={{ marginBottom: '50px' }}>
+                        <div className="card-body">
+                            <div id="example2_wrapper" className="dataTables_wrapper dt-bootstrap4">
+                                <div className="row">
+                                    <div className="col-sm-12 col-md-6" />
+                                    <div className="col-sm-12 col-md-6" />
+                                </div>
+                                <div className="row">
+                                    <div className="col-sm-12">
+                                        <table id="example2" className="table table-bordered table-hover dataTable dtr-inline" role="grid" aria-describedby="example2_info">
+                                            <thead>
+                                                <tr role="row">
+                                                    <th className="sorting sorting_asc th_sort" tabIndex={0} aria-controls="example2" rowSpan={1} colSpan={1} aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">
+                                                        Tiêu đề
+                                                        <i className="fa fa-filter" aria-hidden="true" />
+                                                    </th>
+                                                    <th className="sorting th_sort" tabIndex={0} aria-controls="example2" rowSpan={1} colSpan={1} aria-label="Browser: activate to sort column ascending">
+                                                        Biểu tượng
+                                                    </th>
+                                                    <th className="sorting th_sort th1" tabIndex={0} aria-controls="example2" rowSpan={1} colSpan={1} aria-label="Platform(s): activate to sort column ascending">
+                                                        Ngày tạo
+                                                        <i className="fa fa-filter" aria-hidden="true" />
+                                                    </th>
+                                                    <th className="sorting th_sort th" tabIndex={0} aria-controls="example2" rowSpan={1} colSpan={1} aria-label="Engine version: activate to sort column ascending">
+                                                        Trạng thái
+                                                    </th>
+                                                    <th className="sorting th_sort" tabIndex={0} aria-controls="example2" rowSpan={1} colSpan={1} aria-label="CSS grade: activate to sort column ascending">
+                                                        Thao tác
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr className="odd">
+                                                    <td className="dtr-control sorting_1" tabIndex={0}>Gecko</td>
+                                                    <td> <img src="./assets/images/logo512.png" alt="logo" className="images" /> </td>
+                                                    <td className="th1">28-02-2000 19:00:15</td>
+                                                    <td className="th">Đăng</td>
+                                                    <td>
+                                                        <button type="button" className="btn btn-block btn-danger btn-sm"><i className="fa fa-trash" aria-hidden="true" /></button>
+                                                        &nbsp;|&nbsp;
+                                                        <button type="button" className="btn btn-block btn-success btn-sm"><i className="fa fa-pencil" aria-hidden="true" /></button>
+                                                    </td>
+                                                </tr>
+                                                
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-sm-12 col-md-12">
+                                        <div className="dataTables_paginate paging_simple_numbers" id="example2_paginate">
+                                            <ul className="pagination">
+                                                <li className="paginate_button page-item previous disabled" id="example2_previous"><a href="#" aria-controls="example2" data-dt-idx={0} tabIndex={0} className="page-link">Previous</a>
+                                                </li>
+                                                <li className="paginate_button page-item active"><a href="#" aria-controls="example2" data-dt-idx={1} tabIndex={0} className="page-link">1</a></li>
+                                                <li className="paginate_button page-item "><a href="#" aria-controls="example2" data-dt-idx={2} tabIndex={0} className="page-link">2</a></li>
+                                                <li className="paginate_button page-item "><a href="#" aria-controls="example2" data-dt-idx={3} tabIndex={0} className="page-link">3</a></li>
+                                                <li className="paginate_button page-item "><a href="#" aria-controls="example2" data-dt-idx={4} tabIndex={0} className="page-link">4</a></li>
+                                                <li className="paginate_button page-item th"><a href="#" aria-controls="example2" data-dt-idx={5} tabIndex={0} className="page-link">5</a></li>
+                                                <li className="paginate_button page-item th"><a href="#" aria-controls="example2" data-dt-idx={6} tabIndex={0} className="page-link">6</a></li>
+                                                <li className="paginate_button page-item next" id="example2_next"><a href="#" aria-controls="example2" data-dt-idx={7} tabIndex={0} className="page-link">Next</a></li>
+                                            </ul>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            {/* /.card-header */}
-                            <div className="card-body table-responsive p-0" style={{ height: '300px' }}>
-                                <table className="table table-striped table-head-fixed text-nowrap">
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th className="th">Tiêu đề</th>
-                                            <th className="th">Biểu tượng</th>
-                                            <th className="th">Số lượng</th>
-                                            <th className="th">Thời gian tạo</th>
-                                            <th className="th">Trạng thái</th>
-                                            <th className="th_239">Thao tác</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <WorkItem />
-                                        <WorkItem />
-                                        <WorkItem />
-                                    </tbody>
-                                </table>
-                            </div>
                         </div>
                     </div>
-
-                    {/* {/* ------ pagination ------ * /} */}
-                    <div className="pagination">
-                        <nav aria-label="..." className="pagination_nav">
-                            <ul className="pagination">
-                                <li className="page-item disabled">
-                                    <a className="page-link" href="/saas">Previous</a>
-                                </li>
-                                <li className="page-item"><a className="page-link" href="/fdgdfg">1</a></li>
-                                <li className="page-item active" aria-current="page">
-                                    <a className="page-link" href="/sasd">2</a>
-                                </li>
-                                <li className="page-item"><a className="page-link" href="/ds">3</a></li>
-                                <li className="page-item">
-                                    <a className="page-link" href="/sdf">Next</a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                    {/* ------------- end content ----------------- */}
-                    <div className="footer">
-                        CoppyRight @ by ABC
-                    </div>
                 </div>
+                <Footer />
             </div>
         </div>
     );
